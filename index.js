2472a29cf569fc93bf4cab35710e4a496837e495
bold-mixer-0681 <renee.pujol@gmail.com>
2020-12-03 01:49:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  let resultArr = [];
+  for(let m = 0; m < musicians.length; m++){
+
+    for(let i = 0; i < instruments.length; i++){
+        if( m === i){
+        resultArr.push(`${musicians[m]} plays ${instruments[i]}`)
+        }
+      }
+    }
+  return resultArr;
+}
+
+
+function johnLennonFacts(facts){
+  let factsArr = [];
+  let f = 0;
+
+  while(f<facts.length){
+    factsArr.push(`${facts[f]}!!!`);
+    f++;
+    }
+  return factsArr;
+}
+
+function iLoveTheBeatles(p){
+  let newArr = [];
+
+  do {
+    newArr.push("I love the Beatles!");
+    p++;
+  } while (p < 15);
+
+  return newArr;
+}
